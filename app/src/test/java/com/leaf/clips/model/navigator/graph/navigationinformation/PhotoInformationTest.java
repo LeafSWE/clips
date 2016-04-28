ac706d23bb7eaaa9cdc78171382f8fff4823aec5
oeconti <octatux@gmail.com>
2016-04-28 10:03:21 +0200

    Added PhotoInformationTest

---
+++ app/src/test/java/com/leaf/clips/model/navigator/graph/navigationinformation/PhotoInformationTest.java
@@ -0,0 +1,20 @@
+package com.leaf.clips.model.navigator.graph.navigationinformation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Oscar Elia Conti
+ * @version 0.01
+ * @since 0.00
+ */
+public class PhotoInformationTest {
+
+    private PhotoInformation photoInformation;
+
+    @Test
+    public void testGetPhotoInformation() throws Exception {
+        //TODO
+    }
+}
