8aacdd21b74199d109f42cbe6b356cdb6f2bd57a
mzanella <zanna0150@gmail.com>
2016-05-06 17:38:00 +0200

    Modifica all'interfaccia PoiCategoryView

--- app/src/main/java/com/leaf/clips/view/PoiCategoryView.java
+++ app/src/main/java/com/leaf/clips/view/PoiCategoryView.java
@@ -1,9 +1,21 @@
 package com.leaf.clips.view;
 
 /**
- * @author Oscar Elia Conti
- * @version 0.01
- * @since 0.00
+ * @author Marco Zanella
+ * @version 0.02
+ * @since 0.01
+ */
+
+/**
+ *Interfaccia che espone i metodi per aggiornare la UI contenente la lista dei POI appartenenti ad una data categoria
  */
 public interface PoiCategoryView {
+
+    /**
+     * Metodo utilizzato per visualizzare tutti i POI appartenenti ad una certa categoria
+     * @param toDisplay Array di stringhe che rappresentano le categorie che devono essere mostrate
+     */
+    void setPoiListAdapter(String[] toDisplay);
+
 }
+
