0b08495eae9df54d45a7e7e81c1a08b7d2ce8fde
oeconti <octatux@gmail.com>
2016-05-06 10:56:24 +0200

    Added MainDeveloperView

---
+++ app/src/main/java/com/leaf/clips/view/MainDeveloperView.java
@@ -0,0 +1,18 @@
+package com.leaf.clips.view;
+
+import android.widget.Adapter;
+
+/**
+ * @author Oscar Elia Conti
+ * @version 0.01
+ * @since 0.00
+ */
+public interface MainDeveloperView {
+    /**
+     * Metodo utilizzato per visualizzare la lista di tutti i log salvati in locale
+     * @param adp Collegamento tra la lista dei log e la view in cui essi devono essere mostrati
+     * @return  void
+     */
+    void setLogsAdapter(Adapter adp);
+
+}
