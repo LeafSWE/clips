227d185de48714b13aa02681b8dd1460a47fe052
mzanella <zanna0150@gmail.com>
2016-05-07 17:32:06 +0200

    Create interfaccie Listener e InformationListener e modificata interfaccia NavigationListener

---
+++ app/src/main/java/com/leaf/clips/model/InformationListener.java
@@ -0,0 +1,49 @@
+package com.leaf.clips.model;
+
+/**
+ * @author Marco Zanella
+ * @version 0.01
+ * @since 0.01
+ */
+
+/**
+ * Interfaccia che deve essere implementata da chi vuole usufruire delle informazioni contenute
+ * nel model
+ */
+public interface InformationListener {
+
+    /**
+     * Metodo che viene chiamato quando viene recuperata una mappa e non si siano verificate
+     * eccezioni
+     */
+    void onDatabaseLoaded();
+
+    /**
+     * Metodo che viene richiamato nel caso in cui la mappa richiesta non sia stata trovata nel
+     * database locale
+     * @param major Identificativo associato ad un edificio di cui non si possiede la mappa
+     */
+    void noLocalMapFound(int major);
+
+    /**
+     * Metodo che viene richiamato nel caso in cui la mappa richiesta non sia stata trovata nel
+     * database remoto
+     * @param major Identificativo associato ad un edificio di cui non è presente la mappa nel database
+     * remoto
+     */
+    void noRemoteMapFound(int major);
+
+    /**
+     * Metodo che viene invocato nel caso in cui non si riescano a reperire le informazioni sulle
+     * mappe dal database remoto
+     */
+    void cannotRetrieveRemoteMapDetails();
+
+    /**
+     * Metodo che viene invocato nel caso in cui la versione della mappa locale differisca dalla
+     * versione della mappa nel databaase remoto(l'ultima versione disponibile)
+     * @param major Identificativo associato all'edificio di cui non si possiedde la mappa
+     * aggiornata
+     */
+    void noLastaMapVersion(int major);
+}
