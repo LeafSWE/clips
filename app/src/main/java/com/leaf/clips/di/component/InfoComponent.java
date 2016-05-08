d9841cfc818b5bee60c9d5d32aacc97306cef1d2
mzanella <zanna0150@gmail.com>
2016-05-08 10:06:26 +0200

    Aggiunti commenti alle classi del package di

--- app/src/main/java/com/leaf/clips/di/component/InfoComponent.java
+++ app/src/main/java/com/leaf/clips/di/component/InfoComponent.java
@@ -15,8 +15,19 @@ import dagger.Component;
  * @version 0.01
  * @since 0.01
  */
+
+/**
+ * Interfaccia che viene implementata in autonomia da Dagger2 nella quale devono essere dichiarati
+ * i metodi inject che richiedono come parametro la classe in cui sono presenti campi dati annotati
+ * con Inject. Con l'annotazione Component devono essere dichiarate le classi annotate con
+ * l'annotazione Module che permettono di risolvere le dipendenze
+ */
 @Singleton
 @Component(modules = {AppModule.class, InfoModule.class, DatabaseModule.class, SettingModule.class})
 public interface InfoComponent {
+    /**
+     * Metodo che permette di iniettare i campi annotati con Inject negli oggetti di tipo HomeActivity
+     * @param mainActivity Oggetto in cui devono essere iniettate le dipendenze
+     */
     void inject(HomeActivity mainActivity);
 }
