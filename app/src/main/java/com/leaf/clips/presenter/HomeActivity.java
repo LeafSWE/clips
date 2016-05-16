d8285d46faba8ffaa19aece78ce11b9210e5d39c
tfederico <federicotavella.7@gmail.com>
2016-05-16 10:04:44 +0200

    Aggiunti permessi di storage e searchbox

--- app/src/main/java/com/leaf/clips/presenter/HomeActivity.java
+++ app/src/main/java/com/leaf/clips/presenter/HomeActivity.java
@@ -148,6 +148,7 @@ public class HomeActivity extends AppCompatActivity implements InformationListen
 
             builder.create().show();
         }
+        checkStoragePermissions();
     }
 
     /**
@@ -173,6 +174,66 @@ public class HomeActivity extends AppCompatActivity implements InformationListen
                     });
             builder.create().show();
         }
+
+    }
+
+    /**
+     * Controlla che i servizi di Localizzazione siano attivi. In caso negativo chiede all'utente di
+     * attivarli.
+     */
+    public void checkStoragePermissions(){
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        0);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
+        /*if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        0);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }*/
     }
 
     /**
