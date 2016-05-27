5eadd2ef38846c80fdf3de18fabdba18e5e2bdfa
oeconti <octatux@gmail.com>
2016-05-27 14:28:42 +0200

    Added RemoteMapManagerAdapter

---
+++ app/src/main/java/com/leaf/clips/presenter/RemoteMapManagerAdapter.java
@@ -0,0 +1,35 @@
+package com.leaf.clips.presenter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * @author Oscar Elia Conti
+ * @version 0.01
+ * @since 0.00
+ */
+
+// TODO: 5/27/16 Asta + Tracy, classe nuova 
+
+public class RemoteMapManagerAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
