f1837f0d079599c65ee2808dd3d5465af3fa993c
oeconti <octatux@gmail.com>
2016-05-28 21:56:51 +0200

    Added constructor + attribute to MapDownloaderViewImp

--- app/src/main/java/com/leaf/clips/view/MapDownloaderViewImp.java
+++ app/src/main/java/com/leaf/clips/view/MapDownloaderViewImp.java
@@ -8,24 +8,40 @@ package com.leaf.clips.view;
 
 import android.widget.Adapter;
 
+import com.leaf.clips.model.dataaccess.service.DatabaseService;
+import com.leaf.clips.presenter.MapDownloaderActivity;
+
+import java.util.Map;
+
+import javax.inject.Inject;
+
 /**
  *Interfaccia che espone i metodi per aggiornare la UI che mostra il progresso durante il download della mappa di un edificio dal server
  */
 
 public class MapDownloaderViewImp implements MapDownloaderView {
 
+    // TODO: 5/28/16 Aggiungere campi + Costruttore su Asta + Tracy
+    private MapDownloaderActivity presenter;
+    
+    MapDownloaderViewImp(MapDownloaderActivity presenter){
+        this.presenter = presenter;
+    }
+    
     /**
      * Metodo utilizzato per visualizzare la mappa che si sta scaricando
      * @param adp Collegamento tra la mappa che si sta scaricando e la view in cui essa deve essere mostrata
      * @return  void
      */
+    @Override
     public void setDowloadingMap(Adapter adp){}
 
-    // TODO: 5/28/16 Cambiato nome metodo, correggere Asta + Tracy 
+    // TODO: 5/28/16 Cambiato nome metodo, correggere Asta + Tracy
     /**
      * Metodo utilizzato per visualizzare il progresso nel download di una mappa
      * @param prg Attuale progresso del download
      * @return  void
      */
+    @Override
     public void setProgressDownload(int prg){}
 }
