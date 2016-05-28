ba7c7ed7fe02cab82c996dd7eded43d8d824366d
oeconti <octatux@gmail.com>
2016-05-28 21:39:32 +0200

    Added MapDownloader View + Activity

---
+++ app/src/main/java/com/leaf/clips/presenter/MapDownloaderActivity.java
@@ -0,0 +1,33 @@
+package com.leaf.clips.presenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.leaf.clips.R;
+import com.leaf.clips.view.MapDownloaderView;
+
+public class MapDownloaderActivity extends AppCompatActivity {
+
+    /**
+     * View associata a tale Activity
+     */
+    private MapDownloaderView view;
+
+    /**
+     * Metodo che gestisce la View per visualizzare il completamento del download di una mappa
+     * @return  void
+     */
+    public void downloadFinished(){}
+
+    /**
+     * Metodo che inizializza la View associata
+     * @param bundle Componente per salvare lo stato dell'applicazione
+     * @return  void
+     */
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_map_dowloader);
+    }
+
+}
---
+++ app/src/main/java/com/leaf/clips/view/MapDownloaderView.java
@@ -0,0 +1,29 @@
+package com.leaf.clips.view;
+
+/**
+ * @author Oscar Elia Conti
+ * @version 0.01
+ * @since 0.00
+ */
+
+import android.widget.Adapter;
+
+/**
+ *Interfaccia che espone i metodi per aggiornare la UI che mostra il progresso durante il download della mappa di un edificio dal server
+ */
+
+public interface MapDownloaderView {
+    /**
+     * Metodo utilizzato per visualizzare la mappa che si sta scaricando
+     * @param adp Collegamento tra la mappa che si sta scaricando e la view in cui essa deve essere mostrata
+     * @return  void
+     */
+    void setDowloadingMap(Adapter adp);
+
+    /**
+     * Metodo utilizzato per visualizzare il progresso nel download di una mappa
+     * @param prg Attuale progresso del download
+     * @return  void
+     */
+    void setProgressDowload(int prg);
+}
