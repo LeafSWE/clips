21b12ab8ff8444133bb6c0f7579780f0f6b952a3
oeconti <octatux@gmail.com>
2016-05-30 01:00:02 +0200

    Modified JavaDoc on MapDownloaderView and Imp and Removed TODO

--- app/src/main/java/com/leaf/clips/view/MapDownloaderViewImp.java
+++ app/src/main/java/com/leaf/clips/view/MapDownloaderViewImp.java
@@ -1,20 +1,14 @@
 package com.leaf.clips.view;
 
+import android.widget.Adapter;
+import com.leaf.clips.R;
+import com.leaf.clips.presenter.MapDownloaderActivity;
+
 /**
  * @author Oscar Elia Conti
  * @version 0.01
  * @since 0.00
  */
-
-import android.widget.Adapter;
-
-import com.leaf.clips.R;
-import com.leaf.clips.model.dataaccess.service.DatabaseService;
-import com.leaf.clips.presenter.MapDownloaderActivity;
-
-import java.util.Map;
-
-import javax.inject.Inject;
 
 /**
  *Interfaccia che espone i metodi per aggiornare la UI che mostra il progresso durante il download della mappa di un edificio dal server
@@ -22,7 +16,9 @@ import javax.inject.Inject;
 
 public class MapDownloaderViewImp implements MapDownloaderView {
 
-    // TODO: 5/28/16 Aggiungere campi + Costruttore su Asta + Tracy
+    /**
+     * Riferimento al presenter che gestisce la view
+     */
     private MapDownloaderActivity presenter;
     
     public MapDownloaderViewImp(MapDownloaderActivity presenter){
@@ -39,7 +35,6 @@ public class MapDownloaderViewImp implements MapDownloaderView {
     @Override
     public void setDowloadingMap(Adapter adp){}
 
-    // TODO: 5/28/16 Cambiato nome metodo, correggere Asta + Tracy
     /**
      * Metodo utilizzato per visualizzare il progresso nel download di una mappa
      * @param prg Attuale progresso del download
