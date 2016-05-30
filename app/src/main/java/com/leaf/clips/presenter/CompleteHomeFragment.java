236df2f9f5d8d03bde63c47530322e4131e4448f
black2279 <black.andrighetto94@gmail.com>
2016-05-30 17:27:31 +0200

    Fix Bug Categorie

--- app/src/main/java/com/leaf/clips/presenter/CompleteHomeFragment.java
+++ app/src/main/java/com/leaf/clips/presenter/CompleteHomeFragment.java
@@ -150,20 +150,13 @@ public class CompleteHomeFragment extends Fragment {
     public void setPoiCategoryListAdapter(List<String> list) {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, list);
 
-        LinearLayout categoryLayout = (LinearLayout) getView();
-        if (categoryLayout != null) {
-            categoryLayout.findViewById(R.id.category_search);
-        }
+        LinearLayout categoryLayout = (LinearLayout)getView().findViewById(R.id.category_search);
 
         //Imposta l'altezza della ListView in modo da mostrarne tutti gli item, evitando scroll interno.
-        ViewGroup.LayoutParams params = null;
-        if (categoryLayout != null) {
-            params = categoryLayout.getLayoutParams();
-        }
+        ViewGroup.LayoutParams params = categoryLayout.getLayoutParams();
+
         //height = altezza_titolo + numero_item * altezza_item
-        if (params != null) {
-            params.height = 100 + list.size()*100;
-        }
+        params.height = 100 + list.size()*100;
 
         poiCategories.setAdapter(adapter);
     }
