d3c635670406607b261b94e45b44c29aecf44dbe
mzanella <zanna0150@gmail.com>
2016-05-30 22:00:15 +0200

    Aggiornamento javadoc CompassListener

--- app/src/main/java/com/leaf/clips/model/compass/CompassListener.java
+++ app/src/main/java/com/leaf/clips/model/compass/CompassListener.java
@@ -7,6 +7,15 @@ package com.leaf.clips.model.compass;
  * @version 0.01
  * @since 0.01
  */
+
+/**
+ * Interfaccia base che deve essere implementata dalle classi che vogliono leggere i cambiamenti della bussola
+ */
 public interface CompassListener {
+
+    /**
+     * Metodo che viene invocato ad ogni cambiamento della bussola
+     * @param orientation Gradazione rilevata dalla bussola
+     */
     void changed(float orientation);
 }
