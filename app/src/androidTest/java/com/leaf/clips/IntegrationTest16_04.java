4dc80741629d87bdcfc5c33043c04f136632219a
tfederico <federicotavella.7@gmail.com>
2016-06-01 20:38:49 +0200

    Test LoggingActivity

---
+++ app/src/androidTest/java/com/leaf/clips/IntegrationTest16_04.java
@@ -0,0 +1,91 @@
+package com.leaf.clips;
+/**
+ * @author Federico Tavella
+ * @version 0.02
+ * @since 0.00
+ */
+
+import android.os.Build;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.intent.Intents;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
+import android.util.Log;
+
+import com.leaf.clips.presenter.DeveloperUnlockerActivity;
+import com.leaf.clips.presenter.LoggingActivity;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+
+/**
+ * Test che verifica l'apertura di tutte le activity apribili dalla LoggingActivity tramite un click
+ */
+public class IntegrationTest16_04 {
+    LoggingActivity testActivity;
+
+    @Rule
+    public ActivityTestRule<LoggingActivity> mActivityRule =
+            new ActivityTestRule<>(LoggingActivity.class);
+
+    @Before
+    public void init() throws InterruptedException {
+        testActivity = mActivityRule.getActivity();
+
+        UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        if (Build.VERSION.SDK_INT >= 23) {
+
+            UiObject locationPermissions = device.findObject(new UiSelector().text("Consenti"));
+            if(locationPermissions.exists()){
+                try{
+                    locationPermissions.click();
+
+                } catch (UiObjectNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        UiObject allowPermissions = device.findObject(new UiSelector().text("OK"));
+        while (allowPermissions.exists()) {
+            try {
+                allowPermissions.click();
+                UiObject activateLocation = device.findObject(new UiSelector().text("Non attiva"));
+                if(activateLocation.exists() && !activateLocation.isChecked()){
+                    activateLocation.click();
+                    device.pressBack();
+                }
+
+            } catch (UiObjectNotFoundException e) {
+                Log.e("TEST", "There is no permissions dialog to interact with ");
+            }
+        }
+
+        if(Build.VERSION.SDK_INT >= 23){
+            UiObject storagePermissions = device.findObject(new UiSelector().text("Consenti"));
+            if(storagePermissions.exists()){
+                try{
+                    storagePermissions.click();
+                } catch (UiObjectNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    @Test
+    public void shouldLaunchMainDeveloperIntent(){
+        Intents.init();
+        testActivity.findViewById(R.id.fab_stop_logging).callOnClick();
+        intended(hasComponent(DeveloperUnlockerActivity.class.getName()));
+        Intents.release();
+    }
+}
