e2fbe7a75edf6ef738c64160b0303dbf3df915a4
oeconti <octatux@gmail.com>
2016-06-03 19:22:07 +0200

    Added PreferencesView + Imp to Tracy

--- app/src/main/java/com/leaf/clips/view/PreferencesView.java
+++ app/src/main/java/com/leaf/clips/view/PreferencesView.java
@@ -12,17 +12,6 @@ import android.widget.Adapter;
  *Interfaccia che espone i metodi per aggiornare la UI contenente le preferenze dell'utente rispetto al percorso consigliato e alla modalità di fruizione delle istruzioni di navigazione
  */
 public interface PreferencesView {
-    /**
-     * Metodo utilizzato per visualizzare le preferenze dell'utente riguardo la fruizione delle istruzioni di navigazione
-     * @param adp Collegamento tra le preferenze riguardanti la fruizione delle istruzioni di navigazione e la view in cui esse devono essere mostrate
-     */
-    void setInstructionPreferences(Adapter adp);
-
-    /**
-     * Metodo utilizzato per visualizzare le preferenze dell'utente relative al percorso proposto
-     * @param adp Collegamento tra le preferenze riguardanti le preferenze del percorso di navigazione e la view in cui esse devono essere mostrate
-     */
-    void setPathPreferences(Adapter adp);
 
     /**
      * Metodo utilizzato per visualizzare le preferenze dell'applicazione
--- app/src/main/java/com/leaf/clips/view/PreferencesViewImp.java
+++ app/src/main/java/com/leaf/clips/view/PreferencesViewImp.java
@@ -24,7 +24,6 @@ public class PreferencesViewImp implements PreferencesView, SharedPreferences.On
      */
     private PreferencesActivity presenter;
 
-    // TODO: 17/05/16 aggiungere tracy/uml se va bene
     /**
      * Fragment per mostrare le preferenze
      */
@@ -46,7 +45,6 @@ public class PreferencesViewImp implements PreferencesView, SharedPreferences.On
 
     }
 
-    // TODO: 17/05/16 aggiungere tracy/uml se va bene
     /**
      * Metodo utilizzato per visualizzare le preferenze dell'applicazione
      * @param xmlPreferenceResource Risorsa XML che contiene tutte le preferenze impostabili
@@ -58,7 +56,6 @@ public class PreferencesViewImp implements PreferencesView, SharedPreferences.On
                 .registerOnSharedPreferenceChangeListener(this);
     }
 
-    // TODO: 17/05/16 aggiungere tracy/uml se va bene
     /**
      * Metodo che viene invocato ad ogni cambio di preferenza. Tale cambio viene propagato al presenter
      * che ha il compito di salvarlo.
@@ -72,23 +69,5 @@ public class PreferencesViewImp implements PreferencesView, SharedPreferences.On
             Log.i("setting", "Entra if->OnSharedPreferenceChanged");
             presenter.savePreferences();
         }
-    }
-
-    /**
-     * Metodo utilizzato per visualizzare le preferenze dell'utente riguardo la fruizione delle istruzioni di navigazione
-     * @param adp Collegamento tra le preferenze riguardanti la fruizione delle istruzioni di navigazione e la view in cui esse devono essere mostrate
-     */
-    @Override
-    public void setInstructionPreferences(Adapter adp) {
-        // TODO: 17/05/16 togliere se quanto fatto va bene
-    }
-
-    /**
-     * Metodo utilizzato per visualizzare le preferenze dell'utente relative al percorso proposto
-     * @param adp Collegamento tra le preferenze riguardanti le preferenze del percorso di navigazione e la view in cui esse devono essere mostrate
-     */
-    @Override
-    public void setPathPreferences(Adapter adp) {
-        // TODO: 17/05/16 togliere se quanto fatto va bene
     }
 }
