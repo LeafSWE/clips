21870362e29b9253d79dd2b5a2901071e6017ff8
Davide Castello <davide@moku.io>
2016-06-04 21:02:12 +0200

    TU139

---
+++ app/src/androidTest/java/com/leaf/clips/view/NearbyPoiViewImpTest.java
@@ -0,0 +1,106 @@
+package com.leaf.clips.view;
+
+import android.os.Build;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.contrib.NavigationViewActions;
+import android.support.test.espresso.intent.Intents;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.util.Log;
+import android.view.View;
+
+import com.leaf.clips.R;
+import com.leaf.clips.presenter.HelpActivity;
+import com.leaf.clips.presenter.HomeActivity;
+import com.leaf.clips.presenter.NearbyPoiActivity;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.contrib.DrawerActions.open;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * @author Davide Castello
+ * @version 0.01
+ * @since 0.01
+ *
+ * TU139
+ * Viene verificato che vengano visualizzati tutti i POI associati ad un certo ROI.
+ *
+ */
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class NearbyPoiViewImpTest {
+
+    HomeActivity testActivity;
+
+    @Rule
+    public ActivityTestRule<HomeActivity> mActivityRule =
+            new ActivityTestRule<>(HomeActivity.class);
+
+    @Before
+    public void init() throws InterruptedException {
+        testActivity = mActivityRule.getActivity();
+
+        UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        if (Build.VERSION.SDK_INT >= 23) {
+
+            UiObject locationPermissions = device.findObject(new UiSelector().text("Consenti"));
+            if(locationPermissions.exists()){
+                try{
+                    locationPermissions.click();
+
+                } catch (UiObjectNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        UiObject allowPermissions = device.findObject(new UiSelector().text("OK"));
+        while (allowPermissions.exists()) {
+            try {
+                allowPermissions.click();
+                UiObject activateLocation = device.findObject(new UiSelector().text("Non attiva"));
+                if(activateLocation.exists() && !activateLocation.isChecked()){
+                    activateLocation.click();
+                    device.pressBack();
+                }
+
+            } catch (UiObjectNotFoundException e) {
+                Log.e("TEST", "There is no permissions dialog to interact with ");
+            }
+        }
+
+        if(Build.VERSION.SDK_INT >= 23){
+            UiObject storagePermissions = device.findObject(new UiSelector().text("Consenti"));
+            if(storagePermissions.exists()){
+                try{
+                    storagePermissions.click();
+                } catch (UiObjectNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    @Test
+    public void shouldShowNearbyPOIs(){
+        Intents.init();
+        testActivity.findViewById(R.id.fab_explore_button).setVisibility(View.VISIBLE);
+        testActivity.findViewById(R.id.fab_explore_button).callOnClick();
+        intended(hasComponent(NearbyPoiActivity.class.getName()));
+        Intents.release();
+    }
+}
