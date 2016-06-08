9789017886446a6a8d7e113a57f27ef6188d13bb
Davide Castello <davide@moku.io>
2016-06-08 23:50:06 +0200

    sistemato TU136

--- app/src/androidTest/java/com/leaf/clips/view/NavigationViewImpTest.java
+++ app/src/androidTest/java/com/leaf/clips/view/NavigationViewImpTest.java
@@ -1,37 +1,22 @@
 package com.leaf.clips.view;
 
-import android.content.Context;
-import android.content.Intent;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
-import android.widget.AdapterView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import com.leaf.clips.R;
-import com.leaf.clips.model.InformationManager;
-import com.leaf.clips.model.InformationManagerImp;
-import com.leaf.clips.model.NavigationManager;
-import com.leaf.clips.model.dataaccess.dao.DaoFactoryHelper;
-import com.leaf.clips.model.dataaccess.dao.MapsDbHelper;
-import com.leaf.clips.model.dataaccess.service.DatabaseService;
-import com.leaf.clips.model.dataaccess.service.ServiceHelper;
-import com.leaf.clips.model.navigator.BuildingMap;
 import com.leaf.clips.model.navigator.ProcessedInformation;
 import com.leaf.clips.model.navigator.ProcessedInformationImp;
-import com.leaf.clips.model.navigator.graph.area.PointOfInterest;
-import com.leaf.clips.model.navigator.graph.edge.Edge;
+import com.leaf.clips.model.navigator.graph.area.RegionOfInterest;
+import com.leaf.clips.model.navigator.graph.area.RegionOfInterestImp;
 import com.leaf.clips.model.navigator.graph.edge.EnrichedEdge;
-import com.leaf.clips.presenter.CompleteHomeFragment;
-import com.leaf.clips.presenter.HomeActivity;
+import com.leaf.clips.model.navigator.graph.edge.StairEdge;
+import com.leaf.clips.model.navigator.graph.navigationinformation.BasicInformation;
+import com.leaf.clips.model.navigator.graph.navigationinformation.DetailedInformation;
+import com.leaf.clips.model.navigator.graph.navigationinformation.NavigationInformation;
+import com.leaf.clips.model.navigator.graph.navigationinformation.NavigationInformationImp;
+import com.leaf.clips.model.navigator.graph.navigationinformation.PhotoInformation;
+import com.leaf.clips.model.navigator.graph.navigationinformation.PhotoRef;
 import com.leaf.clips.presenter.NavigationActivity;
-import com.leaf.clips.presenter.PoiCategoryActivity;
-
-import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -40,22 +25,14 @@ import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
-import java.util.ListIterator;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.scrollTo;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.intent.Intents.intended;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -73,87 +50,65 @@ import static org.mockito.Mockito.when;
 public class NavigationViewImpTest {
 
     NavigationActivity testActivity;
-    InformationManager infoManager;
-    NavigationManager navManager;
-    DatabaseService dbService;
-    BuildingMap map;
-    final static int major = 666;
-    final static int end_poi_id = 11;
-    PointOfInterest end_poi;
-    Intent result;
-    NavigationViewImp mockView;
-    List<ProcessedInformation> informations;
 
     @Rule
     public ActivityTestRule<NavigationActivity> mActivityRule =
-            new ActivityTestRule<NavigationActivity>(NavigationActivity.class) {
-                @Override
-                protected Intent getActivityIntent() {
-                    Context targetContext = InstrumentationRegistry.getInstrumentation()
-                            .getTargetContext();
-                    result = new Intent(targetContext, NavigationActivity.class);
-                    result.putExtra("poi_id", end_poi_id);
-                    return result;
-                }
-            };
+            new ActivityTestRule<>(NavigationActivity.class);
 
     @Before
-    public void init() throws Exception {
+    public void setUp() throws Exception {
+
         testActivity = mActivityRule.getActivity();
-        dbService = ServiceHelper.getService(DaoFactoryHelper.getInstance()
-                        .getSQLiteDaoFactory(new MapsDbHelper(testActivity.getApplicationContext())
-                                .getWritableDatabase()),
-                DaoFactoryHelper.getInstance().getRemoteDaoFactory(), MapsDbHelper.REMOTE_DB_URL);
-        map = dbService.findBuildingByMajor(major);
-        boolean found = false;
-        //Trova il POI all'id scelto
-        List<PointOfInterest> poiList = (List<PointOfInterest>) map.getAllPOIs();
-        for(ListIterator<PointOfInterest> i = poiList.listIterator(); i.hasNext() && !found;){
-            PointOfInterest poi = i.next();
-            if(poi.getId() == end_poi_id){
-                end_poi = poi;
-                found = true;
-            }
-        }
-        informations = new LinkedList<>();
-        Iterator<EnrichedEdge> it = map.getAllEdges().iterator();
-        ProcessedInformation info1 = new ProcessedInformationImp(it.next());
-        ProcessedInformation info2 = new ProcessedInformationImp(it.next());
-        ProcessedInformation info3 = new ProcessedInformationImp(it.next());
-        informations.add(info1);
-        informations.add(info2);
-        informations.add(info3);
-
-
-        mockView = Mockito.mock(NavigationViewImp.class);
-        infoManager = Mockito.mock(InformationManager.class);
-        when(infoManager.getBuildingMap()).thenReturn(map);
-
-        navManager = Mockito.mock(NavigationManager.class);
-        when(navManager.getAllNavigationInstruction()).thenReturn(informations);
-        when(navManager.startNavigation(end_poi)).thenReturn(null);
-        //when(navManager.addListener()).then
-
-        Field field = testActivity.getClass().getDeclaredField("informationManager");
-        Field field2 = testActivity.getClass().getDeclaredField("navigationManager");
-        Field field3 = testActivity.getClass().getDeclaredField("view");
-        field.setAccessible(true);
-        field2.setAccessible(true);
-        field3.setAccessible(true);
-        field.set(testActivity, infoManager);
-        field2.set(testActivity, navManager);
-        field3.set(testActivity, mockView);
-
     }
 
     @Test
     public void shouldShowNavigationInformation() throws Exception {
-        Class[] cArg = new Class[1];
-        cArg[0] = Intent.class;
-        Method method = testActivity.getClass().getDeclaredMethod("handleIntent", cArg);
-        method.setAccessible(true);
-        //method.invoke(testActivity, result);
-        // it shows
-        Assert.assertEquals(true, true);
+        List<ProcessedInformation> mockNavigationInstruction;
+        mockNavigationInstruction = Mockito.mock(List.class);
+
+        RegionOfInterest starROI = new RegionOfInterestImp(15, "uuid", 666, 15);
+        RegionOfInterest endROI = new RegionOfInterestImp(15, "uuid", 666, 16);
+
+        BasicInformation basicInformation = new BasicInformation("Basic");
+        DetailedInformation detailedInformation = new DetailedInformation("Detailed");
+
+        PhotoRef photoRef = null;
+        try {
+            photoRef = new PhotoRef(0, new URI("www.google.com"));
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        Collection<PhotoRef> photoRefs = new ArrayList<>();
+        photoRefs.add(photoRef);
+        PhotoInformation photoInformation = new PhotoInformation(photoRefs);
+
+
+        NavigationInformation navigationInformation =
+                new NavigationInformationImp(basicInformation,detailedInformation,photoInformation);
+        EnrichedEdge enrichedEdge = new StairEdge(starROI,endROI,15,15,15,navigationInformation);
+        ProcessedInformation processedInformation = new ProcessedInformationImp(enrichedEdge);
+        List<ProcessedInformation> processedInformations = new ArrayList<>();
+        processedInformations.add(processedInformation);
+        NavigationView mockNavigationView = Mockito.mock(NavigationView.class);
+
+        when(mockNavigationInstruction.size()).thenReturn(0);
+
+
+        Field fieldNavView = null;
+        try {
+            fieldNavView = testActivity.getClass().getDeclaredField("view");
+            fieldNavView.setAccessible(true);
+            fieldNavView.set(testActivity, mockNavigationView);
+            Field fieldNavigationInstruction;
+            fieldNavigationInstruction =
+                    testActivity.getClass().getDeclaredField("navigationInstruction");
+            fieldNavigationInstruction.setAccessible(true);
+            fieldNavigationInstruction.set(testActivity, processedInformations);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        testActivity.informationUpdate(processedInformation);
+        verify(mockNavigationView,times(1)).refreshInstructions(0);
     }
 }
