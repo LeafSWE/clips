7ec80b51133bd8da41fe170f080a03f4b51f76db
tfederico <federicotavella.7@gmail.com>
2016-06-10 16:18:16 +0200

    TI2 funzionante

--- app/src/androidTest/java/com/leaf/clips/IntegrationTest2.java
+++ app/src/androidTest/java/com/leaf/clips/IntegrationTest2.java
@@ -1,12 +1,13 @@
 package com.leaf.clips;
 /**
  * @author Federico Tavella
- * @version 0.01
+ * @version 0.05
  * @since 0.00
  */
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Point;
 import android.net.wifi.WifiManager;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
@@ -21,6 +22,9 @@ import com.leaf.clips.model.NavigationManager;
 import com.leaf.clips.model.NoBeaconSeenException;
 import com.leaf.clips.model.beacon.MyBeacon;
 import com.leaf.clips.model.beacon.MyBeaconImp;
+import com.leaf.clips.model.navigator.NavigationExceptions;
+import com.leaf.clips.model.navigator.graph.area.PointOfInterest;
+import com.leaf.clips.model.navigator.graph.area.PointOfInterestImp;
 import com.leaf.clips.presenter.HomeActivity;
 import com.leaf.clips.presenter.MyApplication;
 import com.leaf.clips.presenter.NavigationActivity;
@@ -29,12 +33,13 @@ import junit.framework.Assert;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.Identifier;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.io.IOException;
+
 import java.lang.reflect.Field;
 import java.util.Collection;
 import java.util.LinkedList;
@@ -60,6 +65,7 @@ public class IntegrationTest2 extends InstrumentationTestCase{
 
     @Before
     public void init() throws NoSuchFieldException, IllegalAccessException {
+
         navigationActivity = mActivityRule.getActivity();
         Field field = NavigationActivity.class.getDeclaredField("informationManager");
         field.setAccessible(true);
@@ -68,34 +74,49 @@ public class IntegrationTest2 extends InstrumentationTestCase{
         field.setAccessible(true);
         navigationManager = (NavigationManager) field.get(navigationActivity);
 
+        WifiManager wifiManager = (WifiManager) InstrumentationRegistry.getTargetContext()
+                .getSystemService(Context.WIFI_SERVICE);
+        wifiManager.setWifiEnabled(true);
+        assertTrue(wifiManager.isWifiEnabled());
+
     }
 
 
-    @Test
-    public void shouldBeAbleToRetrieveRemoteMap() throws InterruptedException {
 
+
+    @Test (expected = NoBeaconSeenException.class)
+    public void shouldNotHaveMap() throws NoBeaconSeenException {
+        informationManager.getBuildingMap();
+    }
+
+    @Test
+    public void shouldRetrieveMapAccessDataAndDeleteMap()
+            throws InterruptedException, NoBeaconSeenException {
+
+        WifiManager wifiManager = (WifiManager) InstrumentationRegistry.getTargetContext()
+                .getSystemService(Context.WIFI_SERVICE);
+        if(!wifiManager.isWifiEnabled())
+            wifiManager.setWifiEnabled(true);
+        assertTrue(wifiManager.isWifiEnabled());
         PriorityQueue<MyBeacon> p = new PriorityQueue<>();
 
         List<Long> list = new LinkedList<>();
         list.add((long) 12);
         UUID uuid = UUID.fromString(MyApplication.getConfiguration().getApplicationUUID());
 
-        p.add(new MyBeaconImp(new Beacon.Builder().setId1(uuid.toString()).setId2("666").setId3("0").setDataFields(list).build()));
+        p.add(new MyBeaconImp(new Beacon.Builder().setId1(uuid.toString()).setId2("666").setId3("0")
+                .setDataFields(list).build()));
 
         Intent msg = new Intent("beaconsDetected");
         msg.putExtra("queueOfBeacons", p);
         LocalBroadcastManager.getInstance(InstrumentationRegistry.getTargetContext()).sendBroadcast(msg);
 
-        Thread.sleep(1000);
-        assertNotNull(informationManager.getAllVisibleBeacons().peek());
-        WifiManager wifiManager = (WifiManager) InstrumentationRegistry.getTargetContext()
-                .getSystemService(Context.WIFI_SERVICE);
-        wifiManager.setWifiEnabled(true);
-        assertTrue(wifiManager.isWifiEnabled());
+        Thread.sleep(500);
+        assertNotNull(informationManager.getAllVisibleBeacons().iterator().next());
 
         informationManager.downloadMapOfVisibleBeacons(true);
 
-        Thread.sleep(1000);
+        Thread.sleep(500);
 
         try {
             assertNotNull(informationManager.getBuildingMap());
@@ -108,43 +129,37 @@ public class IntegrationTest2 extends InstrumentationTestCase{
             assertNotNull(category);
         }
 
-        wifiManager.setWifiEnabled(false);
-    }
+        assertTrue(informationManager.getDatabaseService().isBuildingMapPresent(666));
+        informationManager.getDatabaseService().deleteBuilding(informationManager.getBuildingMap());
+        assertFalse(informationManager.getDatabaseService().isBuildingMapPresent(666));
 
-    @Test (expected = NoBeaconSeenException.class)
-    public void shouldNotHaveMap() throws NoBeaconSeenException {
-        informationManager.getBuildingMap();
     }
 
     @Test
-    public void shouldRetrieveAndDeleteMap() throws IOException, InterruptedException, NoBeaconSeenException {
+    public void shouldUseCompassWithoutExceptions() throws InterruptedException {
+        navigationManager.startCompass();
+        Thread.sleep(1000);
+        navigationManager.stopCompass();
+    }
+
+    @Test (expected = NavigationExceptions.class)
+    public void shouldNotStartNavigationWithFakePOI()
+            throws NavigationExceptions, NoBeaconSeenException, InterruptedException {
         PriorityQueue<MyBeacon> p = new PriorityQueue<>();
 
         List<Long> list = new LinkedList<>();
         list.add((long) 12);
         UUID uuid = UUID.fromString(MyApplication.getConfiguration().getApplicationUUID());
 
-        p.add(new MyBeaconImp(new Beacon.Builder().setId1(uuid.toString()).setId2("666").setId3("0").setDataFields(list).build()));
+        p.add(new MyBeaconImp(new Beacon.Builder().setId1(uuid.toString()).setId2("666").setId3("0")
+                .setDataFields(list).build()));
 
         Intent msg = new Intent("beaconsDetected");
         msg.putExtra("queueOfBeacons", p);
         LocalBroadcastManager.getInstance(InstrumentationRegistry.getTargetContext()).sendBroadcast(msg);
 
-        Thread.sleep(1000);
-        assertNotNull(informationManager.getAllVisibleBeacons().peek());
-        WifiManager wifiManager = (WifiManager) InstrumentationRegistry.getTargetContext()
-                .getSystemService(Context.WIFI_SERVICE);
-        wifiManager.setWifiEnabled(true);
-        assertTrue(wifiManager.isWifiEnabled());
-
-        informationManager.downloadMapOfVisibleBeacons(false);
-
-        Thread.sleep(1000);
-
-        informationManager.getDatabaseService().findRemoteBuildingByMajor(666);
-        assertTrue(informationManager.getDatabaseService().isBuildingMapPresent(666));
-        informationManager.getDatabaseService().deleteBuilding(informationManager.getBuildingMap());
-        assertFalse(informationManager.getDatabaseService().isBuildingMapPresent(666));
+        Thread.sleep(500);
+        PointOfInterest poi = new PointOfInterestImp(3456,null);
+        navigationManager.startNavigation(poi);
     }
-
 }
