4326a9bb6f909706f91f1de6a3bbcddca074299d
tfederico <federicotavella.7@gmail.com>
2016-06-13 18:16:41 +0200

    aggiunto RunWith

--- app/src/test/java/com/leaf/clips/model/dataaccess/dao/EdgeTypeTableTest.java
+++ app/src/test/java/com/leaf/clips/model/dataaccess/dao/EdgeTypeTableTest.java
@@ -4,6 +4,8 @@ import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.*;
 /**
@@ -18,6 +20,7 @@ import static org.junit.Assert.*;
  * TU76
  */
 
+@RunWith(JUnit4.class)
 public class EdgeTypeTableTest {
 
     private int id;
