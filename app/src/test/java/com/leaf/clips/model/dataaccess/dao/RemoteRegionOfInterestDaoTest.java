0fd5da8d27e9c65904e22496045bf180cf6fd8f2
tfederico <federicotavella.7@gmail.com>
2016-06-13 18:17:21 +0200

    aggiunto RunWith

--- app/src/test/java/com/leaf/clips/model/dataaccess/dao/RemoteRegionOfInterestDaoTest.java
+++ app/src/test/java/com/leaf/clips/model/dataaccess/dao/RemoteRegionOfInterestDaoTest.java
@@ -7,6 +7,8 @@ import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.*;
 
@@ -19,6 +21,7 @@ import static org.junit.Assert.*;
 /**
  * TU65
  */
+@RunWith(JUnit4.class)
 public class RemoteRegionOfInterestDaoTest {
 
     private JsonObject js;
