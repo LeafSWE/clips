414aaa4289f29258ec5e4f4fed42e94fcf089e36
tfederico <federicotavella.7@gmail.com>
2016-06-13 18:17:18 +0200

    aggiunto RunWith

--- app/src/test/java/com/leaf/clips/model/dataaccess/dao/RemotePointOfInterestDaoTest.java
+++ app/src/test/java/com/leaf/clips/model/dataaccess/dao/RemotePointOfInterestDaoTest.java
@@ -7,6 +7,8 @@ import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.*;
 
@@ -19,6 +21,7 @@ import static org.junit.Assert.*;
 /**
  * TU64
  */
+@RunWith(JUnit4.class)
 public class RemotePointOfInterestDaoTest {
 
     private JsonObject js;
