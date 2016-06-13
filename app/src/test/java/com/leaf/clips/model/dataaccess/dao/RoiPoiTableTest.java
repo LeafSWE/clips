3e0aade63676c53c53d8ab0a3391b69085787b03
tfederico <federicotavella.7@gmail.com>
2016-06-13 18:17:27 +0200

    aggiunto RunWith

--- app/src/test/java/com/leaf/clips/model/dataaccess/dao/RoiPoiTableTest.java
+++ app/src/test/java/com/leaf/clips/model/dataaccess/dao/RoiPoiTableTest.java
@@ -4,6 +4,8 @@ import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.*;
 /**
@@ -17,6 +19,7 @@ import static org.junit.Assert.*;
 /**
  * TU74
  */
+@RunWith(JUnit4.class)
 public class RoiPoiTableTest {
     private int poi;
     private int roi;
