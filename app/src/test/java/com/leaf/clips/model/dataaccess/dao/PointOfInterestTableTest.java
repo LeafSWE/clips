a626c2d67f7c85e6d2fa3115a6ad4422a955a68f
tfederico <federicotavella.7@gmail.com>
2016-06-13 18:16:47 +0200

    aggiunto RunWith

--- app/src/test/java/com/leaf/clips/model/dataaccess/dao/PointOfInterestTableTest.java
+++ app/src/test/java/com/leaf/clips/model/dataaccess/dao/PointOfInterestTableTest.java
@@ -4,6 +4,8 @@ import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.*;
 /**
@@ -17,7 +19,7 @@ import static org.junit.Assert.*;
 /**
  * TU72
  */
-
+@RunWith(JUnit4.class)
 public class PointOfInterestTableTest {
 
     private String description;
