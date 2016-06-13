a5ee741b06397ad7355a1f84eb4efee1cbff8468
tfederico <federicotavella.7@gmail.com>
2016-06-13 18:16:20 +0200

    aggiunto RunWith

--- app/src/test/java/com/leaf/clips/IntegrationTest9.java
+++ app/src/test/java/com/leaf/clips/IntegrationTest9.java
@@ -6,12 +6,16 @@ import com.leaf.clips.model.navigator.graph.vertex.VertexImp;
 import junit.framework.Assert;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 /**
  * @author Marco Zanella
  * @version 0.01
  * @since 0.01
  */
+
+@RunWith(JUnit4.class)
 public class IntegrationTest9 {
     @Test
     public void shouldRepresentAVertex() {
